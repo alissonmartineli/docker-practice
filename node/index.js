139e67537b05d0cf14335cf232e5c6f0f5144b0c
Alisson Martineli <martineli.alisson@gmail.com>
2022-05-15 23:11:29 -0300

    feat: working with docker compose

--- node/index.js
+++ node/index.js
@@ -1,7 +1,20 @@
 import express from "express"
+import mysql from "mysql"
 
 const app = express()
 const port = 3000
+
+const config = {
+    host: "db",
+    user: "root",
+    password: "root",
+    database: "nodedb"
+}
+
+const connection = mysql.createConnection(config)
+const sql = "INSERT INTO people (name) VALUES (John')"
+connection.query(sql)
+connection.end()
 
 app.get('/', (_, res) => res.send('Hello World!'))
 
